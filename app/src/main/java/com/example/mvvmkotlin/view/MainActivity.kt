2ace4a4da8f7f9535789854e18ae2c2418f60fea
Kwounsu <sb00lgs@gmail.com>
2020-06-16 08:04:11 -0700

    two editText

--- app/src/main/java/com/example/mvvmkotlin/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkotlin/view/MainActivity.kt
@@ -12,7 +12,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var msgViewModel: MsgViewModel
-    var mMessage:MessageData?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +20,11 @@ class MainActivity : AppCompatActivity() {
         msgViewModel = ViewModelProviders.of(this).get(MsgViewModel::class.java)
 
         button_MVVM.setOnClickListener {
-            mMessage = MessageData(editText_MVVM.text.toString())
-            msgViewModel.getMsg(mMessage!!)
-            Toast.makeText(this, mMessage!!.msg,Toast.LENGTH_SHORT).show()
+            val mMessage = MessageData(editText_1.text.toString())
+            val mMessage2 = MessageData(editText_2.text.toString())
+            msgViewModel.getMsg(mMessage)
+            msgViewModel.getMsg(mMessage2)
+            Toast.makeText(this, mMessage.msg + mMessage2.msg,Toast.LENGTH_SHORT).show()
         }
     }
 }
